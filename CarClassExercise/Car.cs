529405fa28ff8f6004a62ccf7b5273a9d06faa7a
Travis Gersch <t_gersch@hotmail.com>
2022-07-13 21:04:54 -0500

    car class exercise first commit

---
+++ CarClassExercise/Car.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassExercise;
+
+public class Car
+{
+    
+   public Car()
+    {
+
+    }
+    
+    public Car(string make, string model, int year) //parameterized class
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+    }
+
+    public Car(string make, string model, int year, bool used) //overloaded parameterized class
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+        this.Used = used;
+    }
+
+    public bool Used = true;   //field - declared/initialized
+
+    public string Make { get; set; }    //properties - declared with get/set = read/write
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+
+
+    public void PrintCarInfoUsed()
+    {
+        Console.WriteLine("Car Information:");
+        Console.WriteLine($"Make: {Make}");
+        Console.WriteLine($"Model: {Model}");
+        Console.WriteLine($"Year: {Year}");
+        Console.WriteLine($"Used Status: {Used}");
+    }
+
+    public void PrintCarInfoNew()
+    {
+        Console.WriteLine("Car Information:");
+        Console.WriteLine($"Make: {Make}");
+        Console.WriteLine($"Model: {Model}");
+        Console.WriteLine($"Year: {Year}");
+        Console.WriteLine($"Used Status: New");
+    }
+
+
+
+
+
+
+
+
+
+
+}
+    
+    
+    
+    
+
---
+++ CarClassExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using CarClassExercise;
+
+Car car1 = new Car();
+car1.Make = "Chevy";
+car1.Model = "Malibu";
+car1.Year = 2020;
+car1.Used = true;
+Console.WriteLine($"The first car's info is {car1.Make}, {car1.Model}, {car1.Year}, Used: {car1.Used}.");
+Console.WriteLine();
+
+Car car2 = new Car("Toyota", "Prius", 2022);
+car2.PrintCarInfoNew();
+Console.WriteLine();
+
+
+
+Car car3 = new Car() { Make = "Honda", Model = "Civic", Year = 2021, Used = true };
+car3.PrintCarInfoUsed();
+Console.WriteLine();
